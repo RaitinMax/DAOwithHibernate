423d0172e3c1708a8135cf69a154c3b76878eb30
MaxPC <jhosh1212@gmail.com>
2023-07-04 11:46:24 +0700

    Initial commit

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,26 @@
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Controller {
+
+    private final RepositoryPerson repositoryPerson;
+
+    public Controller(RepositoryPerson repositoryPerson) {
+        this.repositoryPerson = repositoryPerson;
+    }
+
+    @GetMapping("/products/fetch-product")
+    @ResponseBody
+    public List getUserName(@RequestParam("city") String city) {
+
+        return repositoryPerson.getPersonsByCity(city);
+    }
+
+}
